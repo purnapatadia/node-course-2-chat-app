a2d02c3fce33130f0e6b54f4e25037a8e39650e0
Rudra gesota <rudra.gesota@theappguruz.in>
2018-07-04 16:44:21 +0530

    Final App

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,17 +11,38 @@ function scrollToBottom() {
     var lastMessageHeight = newMessage.prev().innerHeight();
 
     if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
-     messages.scrollTop(scrollHeight);
+        messages.scrollTop(scrollHeight);
 
     }
 }
 
 socket.on('connect', function () {
     console.log('Connected to server');
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function (err) {
+        if (err) {
+            alert(err);
+            window.location.href = '/';
+        }
+        else {
+            console.log('No error');
+
+        }
+    });
 });
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server');
+});
+
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>');
+
+    users.forEach(function (user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+    jQuery('#users').html(ol);
 });
 
 socket.on('newMessage', function (message) {
@@ -57,7 +78,6 @@ jQuery('#message-form').on('submit', function (e) {
     var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextbox.val()
     }, function () {
         messageTextbox.val('');
